0fc970c5e58fee37190d55937c490700b5179cfa
Олександр Пархоменко <vt211_poye@student.ztu.edu.ua>
2023-09-14 17:47:07 +0300

    Added some authors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>{children}</body>
+      <p>Made By Rak Maniak, Alivka, Sonya and others</p>
     </html>
   );
 }
