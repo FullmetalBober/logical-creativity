bd296ba2b260fb01b02a60b568e02f81e7afae2f
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2023-09-14 20:23:59 +0300

    add react-daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+'use client'; //! Required for use 'react-daisyui'
+
+import { Button } from 'react-daisyui';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Button>Button</Button>
+    </main>
+  );
 }
