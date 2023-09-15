8ae8ef73341f432c2843fc98421abcf546ccdbdb
Andrii Babushko <andriibabushko@gmail.com>
2023-09-15 10:49:06 +0300

    Update main layout.tsx with providers and layout components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Providers from '@/components/providers/Providers';
+import Layout from '@/components/layouts/Layout';
+import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Logical Creativity App',
-  description:
-    'An application for the development of logical thinking skills and creativity.',
+  description: 'An application for the development of logical thinking skills and creativity.',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <Layout>{children}</Layout>
+        </Providers>
+      </body>
     </html>
   );
 }
