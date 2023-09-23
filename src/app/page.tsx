1e5a4784e32a860fc5bed695b5ea40dc843c88c8
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2023-09-23 17:20:15 +0300

    refactor: replace daisyui with nextui | update Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'; //! Required for use 'react-daisyui'
 
-import { Button } from 'react-daisyui';
+import { Button } from '@nextui-org/button';
 
 export default function Home() {
   return (
     <main>
-      <Button>Button</Button>
+      <Button>Click me</Button>
     </main>
   );
 }
