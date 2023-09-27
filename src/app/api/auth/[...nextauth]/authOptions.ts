416e525f033f2143a0451f44e736f9c149cb20b0
AndriiBabushko <vt211_bas@student.ztu.edu.ua>
2023-09-27 11:25:16 +0300

    Change package.json deps

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,8 +1,6 @@
 import { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
-import { prisma } from '@/lib/db';
-import { compare } from 'bcrypt';
 import { User } from '@prisma/client';
 import { axiosInstance } from '@/lib/axios';
 
