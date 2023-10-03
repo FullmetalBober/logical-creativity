126cabcc5ebd24097b772f1b42afe4b9918f514d
AndriiBabushko <vt211_bas@student.ztu.edu.ua>
2023-10-03 21:31:33 +0300

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const endIconInputClasses = 'text-2xl text-default-400 pointer-events-none flex-shrink-0';
+
+export const pageLinks = {
+  home: '/',
+  'new-test': '/new-test',
+  login: '/auth/login',
+  register: '/auth/register',
+};
